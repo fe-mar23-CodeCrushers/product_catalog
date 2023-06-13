3f8502fb24896fce632c299270432e685486b603
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-06-13 19:30:47 +0200

    merge fix

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,25 +1,22 @@
 import './Header.scss';
 
 import { Nav } from '../Nav/Nav';
+import { PageLink } from '../PageLink/PageLink';
 
 export const Header = () => {
   return (
     <header className="header">
       <div className="header__container">
-        <a
-            href="https://fe-mar23-codecrushers.github.io/product_catalog/"
-            className="header__logo"
-            title="Nice Gadgets"
-            target="_blank"
-            rel="noreferrer"
-          >
+        <PageLink 
+          to='/home' 
+          classNames='header__logo' 
+          innerValue={
             <img
               className="header__logo-img"
               src="https://raw.githubusercontent.com/fe-mar23-CodeCrushers/product_catalog/header/public/img/Logo.png"
               alt="Nice Gadgets logo"
             />
-          </a>
-
+          }/>
         <Nav />
      </div>
     </header>
