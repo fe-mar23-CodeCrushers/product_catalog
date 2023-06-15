abe3e7b45fc8e22775fe221f99605beff1d397bf
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-06-15 14:02:22 +0200

    done

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -2,30 +2,32 @@ import { PageLink } from '../PageLink/PageLink';
 import './Navbar.scss';
 
 import { NavButtons } from '../NavButtons/NavButtons';
+import { useLocation } from 'react-router-dom';
 
 interface NavbarProps {
   isOpen: boolean;
 }
 
 export const Navbar: React.FC<NavbarProps> = ({ isOpen }) => {
+  const location = useLocation();
+
   return (
-    <div className={`navbar ${isOpen ? 'navbar--open' : ''}`}
-    >
+    <div className={`navbar ${isOpen ? 'navbar--open' : ''}`}>
       <ul className="navbar__list">
-        <li className="navbar__element navbar__element--active">
-          <PageLink to='home' classNames='navbar__link navbar__link' innerValue='Home'/>
+        <li className={`navbar__element ${location.pathname === '/home' ? 'navbar__element--active' : ''}`}>
+          <PageLink to="/home" classNames="navbar__link" innerValue="Home" />
         </li>
 
-        <li className="navbar__element">
-          <PageLink to='phones' classNames='navbar__link navbar__link' innerValue='Phones'/>
+        <li className={`navbar__element ${location.pathname === '/phones' ? 'navbar__element--active' : ''}`}>
+          <PageLink to="/phones" classNames="navbar__link" innerValue="Phones" />
         </li>
 
-        <li className="navbar__element">
-          <PageLink to='tablets' classNames='navbar__link navbar__link' innerValue='Tablets'/>
+        <li className={`navbar__element ${location.pathname === '/tablets' ? 'navbar__element--active' : ''}`}>
+          <PageLink to="/tablets" classNames="navbar__link" innerValue="Tablets" />
         </li>
 
-        <li className="navbar__element">
-          <PageLink to='accessories' classNames='navbar__link navbar__link' innerValue='Accessories'/>
+        <li className={`navbar__element ${location.pathname === '/accessories' ? 'navbar__element--active' : ''}`}>
+          <PageLink to="/accessories" classNames="navbar__link" innerValue="Accessories" />
         </li>
       </ul>
 
