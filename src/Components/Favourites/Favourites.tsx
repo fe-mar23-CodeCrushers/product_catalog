1e0c94d1373373eeff045c37bcc0d579867fee5f
Mateusz Michałowski <mateusz.michalowski000@gmail.com>
2023-06-16 12:27:08 +0200

    local storage solution

--- src/Components/Favourites/Favourites.tsx
+++ src/Components/Favourites/Favourites.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import { PhoneCard } from "../PhoneCard";
 import { Phone } from "../../types/phone";
@@ -7,24 +8,24 @@ import "./Favourites.scss"
 //In card we need to add onclick function on fav button that execute ToogleFav function with id of desired phone to add/delete
 //Also in header in fav button we need to add that onclick its redirect to <Favourites /> 
 
+localStorage.setItem('fav', JSON.stringify([]));
 
-const fav:Phone[] = [];
-
-
-export const ToggleFav = (id : string) => {
+export function ToggleFav(id : string) {
+    // const fav:Phone[] = [];
+    const [fav, setFav] = useState<Phone[]>([]);
     const [phones, setPhones] = useState<Phone[]>([]);
 
     function adder(id: string) {
         const found = phones.find(phone => phone.id === id);
         if (found !== undefined) {
-            fav.push(found)
+            setFav([...fav,found])
         }
     }
 
     function deleter(id:string) {
         const found = phones.find(phone => phone.id === id);
         if (found !== undefined) {
-            fav.splice(fav.indexOf(found), 1)
+            setFav(fav.filter(element => element !== found));
         }
     }
 
@@ -35,9 +36,22 @@ export const ToggleFav = (id : string) => {
 
     fav.some(phone => phone.id === id) ? deleter(id)
     : adder(id)
+    localStorage.setItem('fav',JSON.stringify(fav));
 }
 
 export const Favourites = () => {
+    const [fav, setFav] = useState<Phone[]>([])
+
+    if (localStorage.getItem('fav') !== null) {
+        setFav(JSON.parse(localStorage.getItem('fav')!))
+    }
+
+
+    useEffect(() => {
+        if (localStorage.getItem('fav') !== null) {
+            setFav(JSON.parse(localStorage.getItem('fav')!))
+        }
+    }, JSON.parse(localStorage.getItem('fav')!))
     return (
         <section className="container">
             {fav.length === 0 && <span className="container__info">There are no favourites</span>}
