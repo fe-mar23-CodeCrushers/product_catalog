93edbd2f52d2f53213b0c4422fafa195ca977bc8
Katarzyna Bogiel <katarzyna.bogiel@poczta.onet.pl>
2023-06-16 12:18:54 +0200

    pagination adapted to mobile screens

--- src/Components/Pagination/Pagination.tsx
+++ src/Components/Pagination/Pagination.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import './Pagination.scss';
 import classNames from 'classnames';
 import { getNumbers } from '../../Helpers/getnumbers';
@@ -23,6 +23,39 @@ export const Pagination: React.FC<PaginationProps> = ({
     isLastPage,
 }) => {
     const range = getNumbers(1, totalPages);
+
+    const selectedRange: number[] = useMemo(() => {
+        let newRange = range;
+        let startIndex = currentPage - 1;
+        let endIndex = currentPage + 3;
+
+        if (totalPages > 4) {
+        newRange = range.slice(startIndex, endIndex);
+        }
+
+        if (currentPage < 5) {
+            newRange = range.slice(0, 4);
+        }
+
+        if (currentPage >= 5 && currentPage < 9) {
+            newRange = range.slice(4, 8);
+        }
+
+        if (currentPage >= 9 && currentPage < 13) {
+            newRange = range.slice(8, 12);
+        }
+
+        if (currentPage >= 13 && currentPage < 17) {
+            newRange = range.slice(12, 16);
+        }
+
+        if (currentPage > totalPages - 3) {
+            newRange = range.slice(-4);
+        }
+    
+        return newRange;
+    }, [range, currentPage, totalPages])
+    
 
     const onPrevious = () => {
         onPageChange(currentPage - 1);
@@ -59,7 +92,7 @@ export const Pagination: React.FC<PaginationProps> = ({
                     <div className="arrow__left"><img src={require(`../../assets/arrow-left.png`)} alt="arrow left"></img></div>
                 </li>
 
-                {range.map(pageNumber => {
+                {selectedRange.map(pageNumber => {
                     return (
                     <li
                         className={classNames('pagination__item', {
