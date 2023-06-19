725428e0add9ad3f389a048cbf09be9b6fb95121
sylcym <prusak.syl@gmail.com>
2023-06-19 18:46:07 +0200

    Toggle burger fix

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -5,30 +5,37 @@ import './Nav.scss';
 export const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className="nav">
       <div className="nav__btns">
         <button
-          className={`nav__burger nav__burger--open `}
-          aria-label="Open navigation menu button"
-          onClick={() => setIsOpen(!isOpen) }
+          className={`nav__burger ${isOpen ? 'nav__burger--open' : ''}`}
+          aria-label="Toggle navigation menu"
+          onClick={toggleMenu}
         >
-          {isOpen 
-            ? (
-              <img
-                src="https://raw.githubusercontent.com/fe-mar23-CodeCrushers/product_catalog/main/public/img/Icons/cross.png"
-                alt="Open navigation menu"
-              />
-            ) : (
-              <img 
-                src="https://raw.githubusercontent.com/fe-mar23-CodeCrushers/product_catalog/main/public/img/Icons/burger.png"
-                alt="Close navigation menu"
-              />
-            )}
+          {isOpen ? (
+            <img
+              src="https://raw.githubusercontent.com/fe-mar23-CodeCrushers/product_catalog/main/public/img/Icons/cross.png"
+              alt="Close navigation menu"
+            />
+          ) : (
+            <img
+              src="https://raw.githubusercontent.com/fe-mar23-CodeCrushers/product_catalog/main/public/img/Icons/burger.png"
+              alt="Open navigation menu"
+            />
+          )}
         </button>
       </div>
 
-      <Navbar isOpen={isOpen} />
+      <Navbar isOpen={isOpen} closeMenu={closeMenu} />
     </nav>
-  )
-}
+  );
+};
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -6,28 +6,53 @@ import { useLocation } from 'react-router-dom';
 
 interface NavbarProps {
   isOpen: boolean;
+  closeMenu: () => void;
 }
 
-export const Navbar: React.FC<NavbarProps> = ({ isOpen }) => {
+export const Navbar: React.FC<NavbarProps> = ({ isOpen, closeMenu }) => {
   const location = useLocation();
+
+  const handleLinkClick = () => {
+    closeMenu();
+  };
 
   return (
     <div className={`navbar ${isOpen ? 'navbar--open' : ''}`}>
       <ul className="navbar__list">
-        <li className={`navbar__element ${location.pathname === '/home' || location.pathname === '/' ? 'navbar__element--active' : ''}`}>
-          <PageLink to="/home" classNames="navbar__link" innerValue="Home" />
+        <li
+          className={`navbar__element ${
+            location.pathname === '/home' || location.pathname === '/'
+              ? 'navbar__element--active'
+              : ''
+          }`}
+          onClick={(handleLinkClick)}
+        >
+          <PageLink
+            to="/home"
+            classNames="navbar__link"
+            innerValue="Home"
+          />
         </li>
 
-        <li className={`navbar__element ${location.pathname === '/phones' ? 'navbar__element--active' : ''}`}>
-          <PageLink to="/phones" classNames="navbar__link" innerValue="Phones" />
+        <li
+          className={`navbar__element ${
+            location.pathname === '/phones' ? 'navbar__element--active' : ''
+          }`}
+          onClick={handleLinkClick}
+        >
+          <PageLink
+            to="/phones"
+            classNames="navbar__link"
+            innerValue="Phones"
+          />
         </li>
 
         {/* <li className={`navbar__element ${location.pathname === '/tablets' ? 'navbar__element--active' : ''}`}>
-          <PageLink to="/tablets" classNames="navbar__link" innerValue="Tablets" />
+          <PageLink to="/tablets" classNames="navbar__link" innerValue="Tablets" onClick={handleLinkClick} />
         </li>
 
         <li className={`navbar__element ${location.pathname === '/accessories' ? 'navbar__element--active' : ''}`}>
-          <PageLink to="/accessories" classNames="navbar__link" innerValue="Accessories" />
+          <PageLink to="/accessories" classNames="navbar__link" innerValue="Accessories" onClick={handleLinkClick} />
         </li> */}
       </ul>
 
